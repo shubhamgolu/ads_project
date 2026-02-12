634cabdbbc109349fa736acd04c122ece301fe46
shubhamgolu <shubham.dubey812@gmail.com>
2026-02-13 01:57:09 +0530

    new change

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -1,17 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { replicate } from "@/lib/replicate";
 export async function POST(req: NextRequest) {
-    const { imageUrl,imageToVideoPrompt,uid } = await req.json();
-
-    const input = {
-    image: imageUrl,
-    prompt: imageToVideoPrompt,
-};
-
-    const output = await replicate.run("wan-video/wan-2.2-i2v-fast", { input });
-// @ts-ignore
-    console.log(output.url);
-    // @ts-ignore
-    return NextResponse.json(output.url);
+   
+    return NextResponse.json("oka");
 
 }
