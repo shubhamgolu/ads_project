3d820e37cac74fb8adb2cfd6a5973e144c83675b
shubhamgolu <shubham.dubey812@gmail.com>
2026-02-13 02:03:38 +0530

    delete userts

--- app/api/user/route.tsx
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { replicate } from "@/lib/replicate";
-export async function POST(req: NextRequest) {
-   
-    return NextResponse.json("oka");
-
-}
